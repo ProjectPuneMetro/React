6f857b9f6c089dfbadd59c3f35399c989b76e2db
ProjectPuneMetro <projectpunemetro@gmail.com>
2023-08-19 11:27:04 +0530

    Update signup.js

--- src/Screens/signup.js
+++ src/Screens/signup.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'; // Make sure to import useHistory if you haven't
+
 import '../Screens/CSS/signup.css';
 
 function SignUp() {
@@ -10,16 +12,16 @@ function SignUp() {
   const history = useHistory();
   const [error, setError] = useState("");
   const userData = {
-    name: name,
-    email: email,
+    user_id:22,
+    user_name: name,
+    user_email: email,
     password: password,
-    mobile: mobile,
+    contact_number: mobile,
   };
-  
+
   const handleNameChange = (e) => {
     setName(e.target.value);
   };
-
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -33,7 +35,9 @@ function SignUp() {
     setMobile(e.target.value);
   };
 
-  const xhr = new XMLHttpRequest();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
@@ -47,39 +51,39 @@ function SignUp() {
       }
     };
 
-    xhr.open("POST", "http://localhost:55955/api/Trains");
+    xhr.open("POST", "http://localhost:55955/api/Users");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(userData));
     console.log(userData);
   };
 
   return (
-    <div style={{ background:"grey",height:"88vh", backgroundSize: 'cover', backgroundPosition: 'center',marginTop:"-20px" }}>
+    <div style={{ background: "grey", height: "88vh", backgroundSize: 'cover', backgroundPosition: 'center', marginTop: "-20px" }}>
       <a href="https://www.google.com/search?q=pune+metro&sxsrf=APwXEdeREmq8bVK_OOf_CrmWw43H0GEexg%3A1684334456129&ei=eOdkZNm9B_7x4-EP5-GciA8&ved=0ahUKEwjZ2cLnyvz-AhX--DgGHecwB_EQ4dUDCA8&uact=5&oq=pune+metro&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIECCMQJzINCAAQigUQsQMQgwEQQzIFCAAQgAQyCwgAEIoFEJECEIsDMhMIABCABBAUEIcCELEDEIMBEIsDMgsIABCKBRCRAhCLAzIICAAQgAQQiwMyCwgAEIoFEJECEIsDMg4IABCABBCxAxCDARCLAzIOCAAQgAQQsQMQgwEQiwM6DQgAEEcQ1gQQyQMQsAM6CggAEEcQ1gQQsAM6CwgAEIoFEJIDELADSgQIQRgAUMQFWMQFYP0JaAFw
-          AXgAgAGLAYgBiwGSAQMwLjGYAQCgAQHIAQm4AQHAAQE&sclient=gws-wiz-serp" target="_blank">
+        AXgAgAGLAYgBiwGSAQMwLjGYAQCgAQHIAQm4AQHAAQE&sclient=gws-wiz-serp" target="_blank">
         <img height="350px" className='imgClass' width="350px" src={logoSrc} alt="Pune Metro Logo" />
       </a>
-      <form target="_blank">
-        <div className="inClass" style={{ width:'500px', height:'500px' ,marginLeft:'800px'}}>
+      <form onSubmit={handleSubmit}>
+        <div className="inClass" style={{ width: '500px', height: '500px', marginLeft: '800px' }}>
           <center>
-            <h2 style={{ fontSize: '40px', fontFamily: 'Poppins, sans-serif', fontWeight: 'bold',color:"black" }} >SIGN UP</h2>
-            <input className="inputClass" type="text" placeholder="Name"  id="name" value={uname} onChange={handleNameChange} required />
+            <h2 style={{ fontSize: '40px', fontFamily: 'Poppins, sans-serif', fontWeight: 'bold', color: "black" }}>SIGN UP</h2>
+            <input className="inputClass" type="text" placeholder="Name" id="name" value={name} onChange={handleNameChange} required />
 
-            <input className="inputClass" type="email" placeholder="Email" id="email" value={uemail} onChange={handleEmailChange}required />
+            <input className="inputClass" type="email" placeholder="Email" id="email" value={email} onChange={handleEmailChange} required />
 
-            <input className="inputClass" type="password" placeholder="Password" id="password" value={upassword} onChange={handlePasswordChange} required />
+            <input className="inputClass" type="password" placeholder="Password" id="password" value={password} onChange={handlePasswordChange} required />
 
             <input className="inputClass" type="password" placeholder="Confirm Password " id="confirmPassword" required />
 
             <input className="inputClass" type="number" placeholder="Mobile" id="mobile" value={mobile} onChange={handleMobileChange} required />
             <br />
 
-            <button className="button-6" style={{marginTop:"10px"}}>Register</button>
+            <button className="button-6" style={{ marginTop: "10px" }} type="submit">Register</button>
           </center>
         </div>
       </form>
     </div>
   );
-
 }
+
 export default SignUp;
